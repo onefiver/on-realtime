5e422afa9b5af637bca4ed51845af3e80658d656
onefiver <joanbq17@gmail.com>
2026-01-03 19:06:33 +0100

    Update version number in fxmanifest.lua
    Bump version from 1.0 to 1.0.1 in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,9 @@ game 'gta5'
 
 author 'onefiver'
 description 'Sync time with real world'
-version '1.0'
+version '1.0.1'
 
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
+
