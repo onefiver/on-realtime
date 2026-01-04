79c3054561fa86d969040a3ce3e6fd30dd00ea53
onefiver <joanbq17@gmail.com>
2026-01-04 02:12:22 +0100

    Change comment syntax from Lua to JavaScript

---
+++ config.js
@@ -0,0 +1,3 @@
+Config = {};
+
+Config.Timezone = "America/Los_Angeles"; // default "America/Los_Angeles" (examples: "Europe/Madrid", "Europe/Paris")
--- config.lua
+++
@@ -1,3 +0,0 @@
-Config = {}
-
-Config.Timezone = "America/Los_Angeles" -- default "America/Los_Angeles" (examples: "Europe/Madrid", "Europe/Paris")
